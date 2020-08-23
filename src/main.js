7e86eed2f360822e787401c50a11772ec3d53274
flora <15071285862@163.com>
2020-08-23 20:36:49 +0800

    add 登录页面

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// 导入全局样式表
+import '@/assets/css/global.css'
+
+import './plugins/element.js'
 Vue.config.productionTip = false
 
 new Vue({
